d51bee5278ea02fc603d1db20ba964f0cc39d392
Jimi Flynn <jimi@suremarketingsolutions.com>
2021-07-16 01:31:40 -0400

    added new dev product images
    changed product components to work with database
    
    modified menu html to print as uppercase
    
    updated firebase rules

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,54 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { redirectUnauthorizedTo, canActivate } from '@angular/fire/auth-guard';
-import { WishlistComponent } from './account/wishlist/wishlist.component';
-import { CartComponent } from './account/cart/cart.component';
 import { DashboardComponent } from './account/dashboard/dashboard.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 import { ForgetPasswordComponent } from './account/forget-password/forget-password.component';
 import { ProfileComponent } from './account/profile/profile.component';
 import { ContactComponent } from './account/contact/contact.component';
-import { CheckoutComponent } from './account/checkout/checkout.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SearchComponent } from './search/search.component';
-import { TypographyComponent } from './typography/typography.component';
-import { ReviewComponent } from './review/review.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { CompareOneComponent } from './compare/compare-one/compare-one.component';
-import { CompareTwoComponent } from './compare/compare-two/compare-two.component';
-import { CollectionComponent } from './collection/collection.component';
-import { LookbookComponent } from './lookbook/lookbook.component';
 import { ErrorComponent } from './error/error.component';
-import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 import { FaqComponent } from './faq/faq.component';
-import { BlogLeftSidebarComponent } from './blog/blog-left-sidebar/blog-left-sidebar.component';
-import { BlogRightSidebarComponent } from './blog/blog-right-sidebar/blog-right-sidebar.component';
-import { BlogNoSidebarComponent } from './blog/blog-no-sidebar/blog-no-sidebar.component';
-import { BlogDetailsComponent } from './blog/blog-details/blog-details.component';
-import { GridTwoComponent } from './portfolio/grid-two/grid-two.component';
-import { GridThreeComponent } from './portfolio/grid-three/grid-three.component';
-import { GridFourComponent } from './portfolio/grid-four/grid-four.component';
-import { MasonryGridTwoComponent } from './portfolio/masonry-grid-two/masonry-grid-two.component';
-import { MasonryGridThreeComponent } from './portfolio/masonry-grid-three/masonry-grid-three.component';
-import { MasonryGridFourComponent } from './portfolio/masonry-grid-four/masonry-grid-four.component';
-import { MasonryFullWidthComponent } from './portfolio/masonry-full-width/masonry-full-width.component';
 import { AuthGuard } from '../core/guards/auth.guard';
 import { VerifyEmailComponent } from './account/verify-email/verify-email.component';
+// import { WishlistComponent } from './account/wishlist/wishlist.component';
+// import { CartComponent } from './account/cart/cart.component';
+// import { CheckoutComponent } from './account/checkout/checkout.component';
+// import { TypographyComponent } from './typography/typography.component';
+// import { ReviewComponent } from './review/review.component';
+// import { OrderSuccessComponent } from './order-success/order-success.component';
+// import { CompareOneComponent } from './compare/compare-one/compare-one.component';
+// import { CompareTwoComponent } from './compare/compare-two/compare-two.component';
+// import { CollectionComponent } from './collection/collection.component';
+// import { LookbookComponent } from './lookbook/lookbook.component';
+// import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+// import { BlogLeftSidebarComponent } from './blog/blog-left-sidebar/blog-left-sidebar.component';
+// import { BlogRightSidebarComponent } from './blog/blog-right-sidebar/blog-right-sidebar.component';
+// import { BlogNoSidebarComponent } from './blog/blog-no-sidebar/blog-no-sidebar.component';
+// import { BlogDetailsComponent } from './blog/blog-details/blog-details.component';
+// import { GridTwoComponent } from './portfolio/grid-two/grid-two.component';
+// import { GridThreeComponent } from './portfolio/grid-three/grid-three.component';
+// import { GridFourComponent } from './portfolio/grid-four/grid-four.component';
+// import { MasonryGridTwoComponent } from './portfolio/masonry-grid-two/masonry-grid-two.component';
+// import { MasonryGridThreeComponent } from './portfolio/masonry-grid-three/masonry-grid-three.component';
+// import { MasonryGridFourComponent } from './portfolio/masonry-grid-four/masonry-grid-four.component';
+// import { MasonryFullWidthComponent } from './portfolio/masonry-full-width/masonry-full-width.component';
 import { map } from 'rxjs/operators';
 
 // const adminOnly = () => hasCustomClaim('admin');
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['pages/login']);
 
 const routes: Routes = [
-  {
-    path: 'wishlist',
-    component: WishlistComponent
-  },
-  {
-    path: 'cart',
-    component: CartComponent
-  },
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -80,11 +72,6 @@ const routes: Routes = [
     component: ContactComponent
   },
   {
-    path: 'checkout',
-    component: CheckoutComponent,
-    ...canActivate(redirectUnauthorizedToLogin)
-  },
-  {
     path: 'aboutus',
     component: AboutUsComponent
   },
@@ -93,89 +80,102 @@ const routes: Routes = [
     component: SearchComponent
   },
   {
-    path: 'typography',
-    component: TypographyComponent
-  },
-  {
-    path: 'review',
-    component: ReviewComponent
-  },
-  {
-    path: 'order/success',
-    component: OrderSuccessComponent
-  },
-  {
-    path: 'compare/one',
-    component: CompareOneComponent
-  },
-  {
-    path: 'compare/two',
-    component: CompareTwoComponent
-  },
-  {
-    path: 'collection',
-    component: CollectionComponent
-  },
-  {
-    path: 'lookbook',
-    component: LookbookComponent
+    path: 'faq',
+    component: FaqComponent
   },
   {
     path: '404',
     component: ErrorComponent
   },
-  {
-    path: 'comingsoon',
-    component: ComingSoonComponent
-  },
-  {
-    path: 'faq',
-    component: FaqComponent
-  },
-  {
-    path: 'blog/left/sidebar',
-    component: BlogLeftSidebarComponent
-  },
-  {
-    path: 'blog/right/sidebar',
-    component: BlogRightSidebarComponent
-  },
-  {
-    path: 'blog/no/sidebar',
-    component: BlogNoSidebarComponent
-  },
-  {
-    path: 'blog/details',
-    component: BlogDetailsComponent
-  },
-  {
-    path: 'portfolio/grid/two',
-    component: GridTwoComponent
-  },
-  {
-    path: 'portfolio/grid/three',
-    component: GridThreeComponent
-  },
-  {
-    path: 'portfolio/grid/four',
-    component: GridFourComponent
-  },
-  {
-    path: 'portfolio/masonry/grid/two',
-    component: MasonryGridTwoComponent
-  },
-  {
-    path: 'portfolio/masonry/grid/three',
-    component: MasonryGridThreeComponent
-  },
-  {
-    path: 'portfolio/masonry/grid/four',
-    component: MasonryGridFourComponent
-  },
-  {
-    path: 'portfolio/masonry/full-width',
-    component: MasonryFullWidthComponent
-  }
+  // {
+  //   path: 'wishlist',
+  //   component: WishlistComponent
+  // },
+  // {
+  //   path: 'cart',
+  //   component: CartComponent
+  // },
+  // {
+  //   path: 'checkout',
+  //   component: CheckoutComponent,
+  //   ...canActivate(redirectUnauthorizedToLogin)
+  // },
+  // {
+  //   path: 'typography',
+  //   component: TypographyComponent
+  // },
+  // {
+  //   path: 'review',
+  //   component: ReviewComponent
+  // },
+  // {
+  //   path: 'order/success',
+  //   component: OrderSuccessComponent
+  // },
+  // {
+  //   path: 'compare/one',
+  //   component: CompareOneComponent
+  // },
+  // {
+  //   path: 'compare/two',
+  //   component: CompareTwoComponent
+  // },
+  // {
+  //   path: 'collection',
+  //   component: CollectionComponent
+  // },
+  // {
+  //   path: 'lookbook',
+  //   component: LookbookComponent
+  // },
+  // {
+  //   path: 'comingsoon',
+  //   component: ComingSoonComponent
+  // },
+  // {
+  //   path: 'blog/left/sidebar',
+  //   component: BlogLeftSidebarComponent
+  // },
+  // {
+  //   path: 'blog/right/sidebar',
+  //   component: BlogRightSidebarComponent
+  // },
+  // {
+  //   path: 'blog/no/sidebar',
+  //   component: BlogNoSidebarComponent
+  // },
+  // {
+  //   path: 'blog/details',
+  //   component: BlogDetailsComponent
+  // },
+  // {
+  //   path: 'portfolio/grid/two',
+  //   component: GridTwoComponent
+  // },
+  // {
+  //   path: 'portfolio/grid/three',
+  //   component: GridThreeComponent
+  // },
+  // {
+  //   path: 'portfolio/grid/four',
+  //   component: GridFourComponent
+  // },
+  // {
+  //   path: 'portfolio/masonry/grid/two',
+  //   component: MasonryGridTwoComponent
+  // },
+  // {
+  //   path: 'portfolio/masonry/grid/three',
+  //   component: MasonryGridThreeComponent
+  // },
+  // {
+  //   path: 'portfolio/masonry/grid/four',
+  //   component: MasonryGridFourComponent
+  // },
+  // {
+  //   path: 'portfolio/masonry/full-width',
+  //   component: MasonryFullWidthComponent
+  // }
 ];
 
 @NgModule({
