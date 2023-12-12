f3b849588d592ba0331af1598a1c0d0f56990d47
Jimi <18079822+jimiDean1337@users.noreply.github.com>
2023-12-11 23:00:26 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 /* TODO: Set live paypal_token */
 export const environment = {
   production: true,
-  instagram_token: 'INSTA_TOKEN',
+  instagram_token: 'INSTAGRAM_TOKEN',
   twitter_token: 'TWITTER_TOKEN',
   facebook_token: 'FACEBOOK_TOKEN',
   stripe_token: 'STRIPE_TOKEN',
-  paypal_token: 'AcNlbd9DD3m82qLVfP9J3hbf8FByrax5NMez84I6v4J7gdbsrCrrBPCQJjOISr6G216KJtGXCQpaROoM',
+  paypal_token: 'PAYPAL_TOKEN',
   firebase_config: {
-    apiKey: "AIzaSyDKFOGrdYVOQTT3DKAWftQZ0Gm5gxGHsDE",
+    apiKey: "API_KEY",
     authDomain: "tai-dye-studios.firebaseapp.com",
     projectId: "tai-dye-studios",
     storageBucket: "tai-dye-studios.appspot.com",
