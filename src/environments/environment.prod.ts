ee970ac502a74d21fb408782d3b90297f3d26b27
Jimi <18079822+jimiDean1337@users.noreply.github.com>
2023-12-11 23:01:28 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  instagram_token: 'IGQVJWSHdITG1HUEtXVVZAvNS13MXdRNnJLSkpKZAEhHYmRHSUkwMlZAPUTlpbFlRVjQxTjJXUzRBSGFKWktwejhseUtud09tUmVmWVhyOGZA0TnU4ZAGYyOG9pUXFzc3JXVGhSbGJYUHpkX21ESXROSF9qcQZDZD',
+  instagram_token: 'INSTAGRAM_TOKEN',
   twitter_token: 'TWITTER_TOKEN',
   facebook_token: 'FACEBOOK_TOKEN',
   stripe_token: 'STRIPE_TOKEN',
-  paypal_token: 'AbHE4suVcPRPFtWYaQ6FMTqmpbeC_qtsfie1TvD986qkV2iqhzPJn7wKbWVRbAHZnOW1dvPkwTLo-hNt',
+  paypal_token: 'PAYPAL_TOKEN',
   firebase_config: {
-    apiKey: "AIzaSyDKFOGrdYVOQTT3DKAWftQZ0Gm5gxGHsDE",
+    apiKey: "API_KEY",
     authDomain: "tai-dye-studios.firebaseapp.com",
     projectId: "tai-dye-studios",
     storageBucket: "tai-dye-studios.appspot.com",
